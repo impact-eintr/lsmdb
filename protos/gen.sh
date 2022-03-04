9e671c3cb3d88ed538694f0aaa813d723c208893
impact-eintr <yixingwei4@gmail.com>
2022-03-04 20:50:09 +0800

    grpc protos

---
+++ protos/gen.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Run this script from its directory, so that badgerpb2.proto is where it's expected to
+# be.
+
+# You might need to go get -v github.com/gogo/protobuf/...
+go get -v github.com/gogo/protobuf/protoc-gen-gogofaster
+protoc --gogofaster_out=. --gogofaster_opt=paths=source_relative -I=. manifest.proto
+protoc --gogofaster_out=. --gogofaster_opt=paths=source_relative -I=. backup.proto
